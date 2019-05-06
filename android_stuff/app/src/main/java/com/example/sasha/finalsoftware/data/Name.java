6c10ffdf03ae834a539fe12b7e4a5d154060f081
Grant Montgomery <montg433@d.umn.edu>
2019-05-06 09:43:13 -0500

    Search Layout
    Search Layout set up

--- android_stuff/app/src/main/java/com/example/sasha/finalsoftware/data/Name.java
+++ android_stuff/app/src/main/java/com/example/sasha/finalsoftware/data/Name.java
@@ -1,4 +1,29 @@
 package com.example.sasha.finalsoftware.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Name {
+    private String name;
+    private String sex;
+    private List<Double> popularityList;
+
+    public Name(String name, String sex, ArrayList<Double> popularityList) {
+        this.name = name;
+        this.sex = sex;
+        this.popularityList = popularityList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public List<Double> getPopularityList() {
+        return popularityList;
+    }
 }
+
