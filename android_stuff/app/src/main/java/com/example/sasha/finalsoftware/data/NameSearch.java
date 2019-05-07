34ae5832c3eb2f50ceb2a435aa11635cc05e7ab1
Tony Madison <madis179@d.umn.edu>
2019-05-06 19:56:26 -0500

    stores names locally when view names is pressed. removed useless classes

--- android_stuff/app/src/main/java/com/example/sasha/finalsoftware/data/NameSearch.java
+++ android_stuff/app/src/main/java/com/example/sasha/finalsoftware/data/NameSearch.java
@@ -1,48 +1,30 @@
 package com.example.sasha.finalsoftware.data;
 
-import android.content.Context;
-import android.support.annotation.NonNull;
-import android.util.Log;
-import android.widget.TextView;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.*;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.support.constraint.Constraints.TAG;
-
 public class NameSearch {
 
     private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-    public Context context;
-    public List nameList;
-    public Name myName;
 
-    public NameSearch(Context context) {
-        this.context = context;
-        nameList = new ArrayList<Name>();
-        myName = new Name();
-    }
-
-    public void getFromFirebase(String name, String yearStart, String yearEnd) {
+    public void getFromFirebase() {
         DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
         ValueEventListener eventListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                List<String> list = new ArrayList<>();
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     Iterable<DataSnapshot> userId = ds.getChildren();
-                    String temp;
-                    temp = ds.child("name").getValue(String.class);
-                    if (temp.equals(name)) {
-                        Log.w("temp is " + temp, "should be " + ds.child("name").getValue(String.class));
+                    for (DataSnapshot uId : userId) {
+                        String data = uId.toString();
+                        list.add(data);
+                        //Log.w("TAG", data);
+
                     }
-                        //Log.w("NAME IS(should be John)", ds.child("name").getValue().toString());
                 }
+                //Log.w("TAG", String.valueOf(list));
             }
 
             @Override
--- android_stuff/app/src/main/java/com/example/sasha/finalsoftware/ui/MainActivity.java
+++ android_stuff/app/src/main/java/com/example/sasha/finalsoftware/ui/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import com.example.sasha.finalsoftware.R;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -27,8 +25,4 @@ public class MainActivity extends AppCompatActivity {
         startActivity(myIntent);
     }
 
-    public void swtichToSearchNames(View myView) {
-        Intent myIntent = new Intent(MainActivity.this, SearchActivity.class);
-        startActivity(myIntent);
-    }
 }
